fc2f1564843bf8c9500f95bc4de8549917b46dad
ThnksCJ <admin@cjstevenson.com>
2022-05-02 12:20:20 +0100

    Added Settings Path (for checking users in DB)

--- index.js
+++ index.js
@@ -15,6 +15,38 @@ app.get('/version', (req, res) => {
     res.send(require('./package.json').version);
 })
 
+app.get('/settings', (req, res) => {
+    res.status(403)
+    res.type('text/plain');
+    res.send("No hwid AND No user")
+})
+
+app.get('/settings/:username', (req, res) => {
+    const name = req.params.username;
+    const hwid = req.query.hwid;
+
+    if(hwid == null) {
+        res.status(403)
+        res.type('text/plain');
+        res.send("No hwid")
+    }
+
+    if(name == null) {
+        res.status(403)
+        res.type('text/plain');
+        res.send("No user")
+    }
+
+    if(name && hwid == null) {
+        res.status(403)
+        res.type('text/plain');
+        res.send("No hwid AND No user")
+    }else{
+        res.type('text/plain');
+        res.send("Yay")
+    }
+})
+
 app.get('/api/1/client/jar', (req, res) => {
     res.status(403)
     res.type('text/plain');
